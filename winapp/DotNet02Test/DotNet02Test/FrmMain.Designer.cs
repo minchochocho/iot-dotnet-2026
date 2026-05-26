97739d59f2001edf1047c9cc34b4b8bf45005e01
minchochocho <150896043+minchochocho@users.noreply.github.com>
2026-05-26 17:27:30 +0900

    1일차

---
+++ winapp/DotNet02Test/DotNet02Test/FrmMain.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DotNet02Test {
+    // partial 키워드를 통해 합침
+
+    partial class FrmMain {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            BtnPress = new Button();
+            LblResult = new Label();
+            SuspendLayout();
+            // 
+            // BtnPress
+            // 
+            BtnPress.Location = new Point(616, 426);
+            BtnPress.Name = "BtnPress";
+            BtnPress.Size = new Size(156, 82);
+            BtnPress.TabIndex = 0;
+            BtnPress.Text = "출력";
+            BtnPress.UseVisualStyleBackColor = true;
+            BtnPress.Click += BtnPress_Click;
+            // 
+            // LblResult
+            // 
+            LblResult.AutoSize = true;
+            LblResult.Location = new Point(349, 259);
+            LblResult.Name = "LblResult";
+            LblResult.Size = new Size(42, 15);
+            LblResult.TabIndex = 1;
+            LblResult.Text = "결과 : ";
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(784, 520);
+            Controls.Add(LblResult);
+            Controls.Add(BtnPress);
+            Font = new Font("맑은 고딕", 9F);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "FrmMain";
+            Text = "첫번째 윈앱";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnPress;
+        private Label LblResult;
+    }
+}
---
+++ winapp/DotNet02Test/DotNet03GuessNum/FrmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace DotNet03GuessNum {
+    partial class FrmMain {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnCheck = new Button();
+            LblDisplay = new Label();
+            BtnStart = new Button();
+            TxtGuess = new TextBox();
+            SuspendLayout();
+            // 
+            // BtnCheck
+            // 
+            BtnCheck.Location = new Point(274, 66);
+            BtnCheck.Name = "BtnCheck";
+            BtnCheck.Size = new Size(83, 30);
+            BtnCheck.TabIndex = 0;
+            BtnCheck.Text = "맞히기";
+            BtnCheck.UseVisualStyleBackColor = true;
+            // 
+            // LblDisplay
+            // 
+            LblDisplay.Dock = DockStyle.Top;
+            LblDisplay.Font = new Font("맑은 고딕", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LblDisplay.Location = new Point(0, 0);
+            LblDisplay.Name = "LblDisplay";
+            LblDisplay.Size = new Size(369, 54);
+            LblDisplay.TabIndex = 1;
+            LblDisplay.Text = "게임을 시작합니다";
+            LblDisplay.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // BtnStart
+            // 
+            BtnStart.Location = new Point(12, 120);
+            BtnStart.Name = "BtnStart";
+            BtnStart.Size = new Size(345, 44);
+            BtnStart.TabIndex = 2;
+            BtnStart.Text = "게임 시작";
+            BtnStart.UseVisualStyleBackColor = true;
+            BtnStart.Click += BtnStart_Click;
+            // 
+            // TxtGuess
+            // 
+            TxtGuess.Font = new Font("맑은 고딕", 12F);
+            TxtGuess.Location = new Point(12, 66);
+            TxtGuess.Name = "TxtGuess";
+            TxtGuess.Size = new Size(256, 29);
+            TxtGuess.TabIndex = 3;
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(369, 176);
+            Controls.Add(TxtGuess);
+            Controls.Add(BtnStart);
+            Controls.Add(LblDisplay);
+            Controls.Add(BtnCheck);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnCheck;
+        private Label LblDisplay;
+        private Button BtnStart;
+        private TextBox TxtGuess;
+    }
+}
